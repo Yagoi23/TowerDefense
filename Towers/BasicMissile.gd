0b501a514fb684597ff6d491a62a5ddb8a395418
Yagoi23 <jeschollum@gmail.com>
2022-10-26 14:45:13 +1300

    Documented Missile

--- Towers/BasicMissile.gd
+++ Towers/BasicMissile.gd
@@ -1,8 +1,6 @@
 extends KinematicBody2D
 class_name BasicMissile
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+
 var target
 var ammo_type
 var proj_speed
@@ -14,32 +12,24 @@ func _physics_process(delta):
 	#print(target + ammo_type + velocity)
 
 func move(delta):
-	var velocity = global_position.direction_to(target.global_position)
-	move_and_slide(velocity*proj_speed*delta)
+	#sets  the direction to the target and then moves towards the target
+	var tar_dir = global_position.direction_to(target.global_position)
+	move_and_slide(tar_dir*proj_speed*delta)
+	#points towards target
 	look_at(target.global_position)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 func _on_Area2D_body_entered(body):
+	#blows up when contacting a missile or enemy
 	if body.is_in_group("enemy"):
 		blow_up()
 	if body.is_in_group("missile"):
 		blow_up()
 
 func blow_up():
+	#creates a new explosiion and sets damage position and friendly damage
 	var new_explosion = load("res://Explosion.tscn").instance()
 	new_explosion.position = self.get_node("Position2D").global_position
 	new_explosion.damage = damage
 	new_explosion.friendly_damage = friendly_damage
-	#new_projectile.proj_speed = GameStats.tower_data[turret_type]["velocity"]
-	#new_projectile.ammo_type = ammo_type
-	#new_projectile.target = target_enemy
-	#new_projectile.damage = GameStats.tower_data[turret_type]["damage"]
-	#new_projectile.friendly_damage = GameStats.tower_data[turret_type]["friendly_damage"]
 	get_parent().add_child(new_explosion)
 	self.queue_free()
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
